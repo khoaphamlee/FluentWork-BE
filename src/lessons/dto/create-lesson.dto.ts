4f81e731b7db3760f3c34386af9fe576d3a20ca0
khoaphamlee <plkhoa0111@gmail.com>
2025-05-10 21:26:15 +0700

    Lesson #1

--- src/lessons/dto/create-lesson.dto.ts
+++ src/lessons/dto/create-lesson.dto.ts
@@ -1,67 +1,39 @@
-import {
-    IsEnum,
-    IsOptional,
-    IsString,
-    MaxLength,
-    IsUrl,
-    IsArray,
-    ArrayNotEmpty,
-    ArrayUnique,
-  } from 'class-validator';
-  
-  export enum LessonFocus {
-    Vocabulary = 'Vocabulary',
-    Grammar = 'Grammar',
-  }
-  
-  export enum LessonSubject {
-    IT = 'IT',
-    Business = 'Business',
-    Finance = 'Finance',
-    Healthcare = 'Healthcare',
-    Hospitality = 'Hospitality',
-  }
-  
-  export enum GrammarPoint {
-    Tense = 'Tense',
-    PassiveVoice = 'Passive Voice',
-    ConditionalSentence = 'Conditional Sentence',
-  }
-  
-  export enum LessonType {
-    Video = 'Video',
-    Audio = 'Audio',
-    Quiz = 'Quiz',
-  }
-  
-  export class CreateLessonDto {
-    @IsOptional()
-    @IsEnum(LessonFocus)
-    lesson_focus?: LessonFocus;
-  
-    @IsOptional()
-    @IsEnum(LessonSubject)
-    subject?: LessonSubject;
-  
-    @IsOptional()
-    @IsEnum(GrammarPoint)
-    grammar_point?: GrammarPoint;
-  
-    @IsArray()
-    @ArrayNotEmpty()
-    @ArrayUnique()
-    @IsEnum(LessonType, { each: true })
-    lesson_type: LessonType[];
-  
-    @IsUrl()
-    content_url: string;
-  
-    @IsString()
-    @MaxLength(255)
-    title: string;
-  
-    @IsString()
-    @MaxLength(1000)
-    description: string;
-  }
-  
+import { IsEnum, IsNotEmpty, IsOptional, IsString, IsInt, MaxLength } from 'class-validator';
+import { Level } from 'src/enum/level.enum';
+import { Topic } from 'src/enum/topic.enum';
+import { VocabularyTopic } from 'src/enum/vocabulary-topic.enum';
+import { GrammarTopic } from 'src/enum/grammar-topic.enum';
+
+export class CreateLessonDto {
+  @IsInt()
+  @IsOptional()
+  defaultOrder?: number;
+
+  @IsString()
+  @MaxLength(255)
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @MaxLength(500)
+  @IsNotEmpty()
+  description: string;
+
+  @IsEnum(Level)
+  level: Level;
+
+  @IsEnum(Topic)
+  type: Topic;
+
+  @IsEnum(VocabularyTopic)
+  @IsOptional()
+  vocabulary_topic?: VocabularyTopic;
+
+  @IsEnum(GrammarTopic)
+  @IsOptional()
+  grammar_topic?: GrammarTopic;
+
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+}
