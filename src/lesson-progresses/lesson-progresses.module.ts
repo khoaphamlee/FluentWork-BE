631c09c8244b0931fb9d3344fbf42b372541417c
Trương Minh Khôi <22520703@gm.uit.edu.vn>
2025-05-11 21:11:39 +0700

    fix bug

--- src/lesson-progresses/lesson-progresses.module.ts
+++ src/lesson-progresses/lesson-progresses.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { LessonProgressesService } from './lesson-progresses.service';
 import { LessonProgressesController } from './lesson-progresses.controller';
+import { TypeORMError } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonProgress } from './entities/lesson-progress.entity';
+import { Lesson } from 'src/lessons/entities/lesson.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([LessonProgress, User, Lesson])],
   controllers: [LessonProgressesController],
   providers: [LessonProgressesService],
+  exports: [LessonProgressesService],
 })
 export class LessonProgressesModule {}
